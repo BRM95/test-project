b24d4a2e598c063bd8b00f76a753113d2430d865
ge46tih <bilal.musani@tum.de>
2021-08-29 14:39:32 +0200

    Updated block comment work item reference

--- src/initPendulum.m
+++ src/initPendulum.m
@@ -1,5 +1,5 @@
-%{
-@wi.implements TP-79 A test requirement for matlab implemented here again
+{%
+@wi.implements  TP-80 A test requirement for matlab block comments 
 %}
 
 g = 9.81; % @wi.implements TP-79 A test requirement for matlab implemented here again

79d90accc99f1d9f365dc12259673660305b12fe
ge46tih <bilal.musani@tum.de>
2021-08-29 15:35:46 +0200

    m

--- src/initPendulum.m
+++ src/initPendulum.m
@@ -13,8 +13,8 @@ K_pole = [-109*g/9,-20*g/9];
 K_lqr = lqr(A,B,eye(2),1);
 L_pole = [40;409];
 [~,L_kalman,~] = kalman(ss(A,[B,eye(2)],C,[0,0,0]),V,W); %{
-             												@wi.implements  TP-81 Another block comment test requirement 
-             											 }%
+                                                         @wi.implements  TP-81 Another block comment test requirement 
+                                                         }%
 
 x0 = [0.1;0];
 xh0 = [0;0];

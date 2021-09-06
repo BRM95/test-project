f91ea9d393405cf697f37bbfcfd7a064fba1374c
ge46tih <bilal.musani@tum.de>
2021-09-06 15:37:17 +0200

    Updated class

--- src/classExample.m
+++ src/classExample.m
@@ -1,14 +1,34 @@
 % @wi.implements TP-91 Class example 
-classdef BasicClass
-   properties
-      Value {mustBeNumeric}
+   classdef mydate
+   % write a description of the class here.
+
+       properties
+       % define the properties of the class here, (like fields of a struct)
+           minute = 0;
+           hour;
+           day;
+           month;
+           year;
+       end
+
+       methods
+       % methods, including the constructor are defined in this block
+
+           function obj = mydate(minute,hour,day,month,year)
+           % class constructor
+               if(nargin > 0)
+                 obj.minute = minute;
+                 obj.hour   = hour;
+                 obj.day    = day;
+                 obj.month  = month;
+                 obj.year   = year;
+               end
+           end
+
+           function obj = rollDay(obj,numdays)
+
+               obj.day = obj.day + numdays;
+           end
+
+       end
    end
-   methods
-      function r = roundOff(obj)
-         r = round([obj.Value],2);
-      end
-      function r = multiplyBy(obj,n)
-         r = [obj.Value] * n;
-      end
-   end
-end
